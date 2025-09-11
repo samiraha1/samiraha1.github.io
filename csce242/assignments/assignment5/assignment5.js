f7374d970081151c27d11c1414888a2d2e7bd43c
samiraha1 <samiraha@email.sc.edu>
2025-09-11 15:30:14 -0400

    jhvikh

--- csce242/assignments/assignment5/assignment5.js
+++ csce242/assignments/assignment5/assignment5.js
@@ -1,4 +1,12 @@
-document.getElementById('img').onClick = () => {
-    console.log('img2')
-}; /*from the intro.js*/
-            img id="img2" src="images/sun.png" style="width:50%";
+document.getElementById("btn3").onclick = () => {
+    const image = document.getElementById("img");
+    console.log(image.src);
+
+    if(image.src.includes("images/cloud1.png")){
+        image.src = "images/sun.png";
+        console.log("Switched to sun.png");
+    } else {
+        image.src = "images/cloud1.png";
+        console.log("switched back")
+    }
+}
