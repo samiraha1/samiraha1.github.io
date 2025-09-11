21b48a208aa7ca8abf049bd358f85dc4cc6fbba8
samiraha1 <samiraha@email.sc.edu>
2025-09-11 19:32:08 -0400

    kjbjk

--- csce242/assignments/assignment5/assignment5.js
+++ csce242/assignments/assignment5/assignment5.js
@@ -10,6 +10,21 @@ document.getElementById("btn3").onclick = () => {
         console.log("switched back")
     }
 }
-document.getElementById("btn1").onclick = () => {
-    const image = document.getElementById("img");
-    console.log(image.src);
+document.getElementById("first1").onclick = () => {
+    const suns = document.getElementById("sun");
+    suns.classList.toggle("hidden");
+    console.log("Toggeled text", Text.classList.contains("hidden"));
+}
+
+const toggle = document.getElementById("btn2");
+const colorPicker = document.getElementById("color-picker");
+const colorText = document.getElementById("color-text");
+
+toggle.onclick = () => {
+    colorPicker.classList.toggle("hidden");
+}
+colorPicker.oninput = (event) => {
+    const chosenColor = event.target.value;
+    colorText.style.color = chosenColor;
+    console.log("Picked color:", chosenColor);
+}
