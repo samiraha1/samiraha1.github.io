ba58c460476d07a03d6e62fd9ca09dbf084b8610
samiraha1 <samiraha@email.sc.edu>
2025-09-19 19:37:51 -0400

    kjbj,

--- csce242/assignments/assignment6/assignment6.js
+++ csce242/assignments/assignment6/assignment6.js
@@ -5,3 +5,25 @@ output.textContent = slider.value + " plantRange";
 slider.oninput = function(){
     output.textContent = this.value + " plantRange";
 };
+
+function updateClock(){
+    const now = new Date();
+
+    let hours = now.getHours();
+    let min = now.getMinutes();
+    let sec = now. getSeconds();
+    let amPm = hours >= 12 ? "pm" : "am";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    min = min < 10 ? "0" + min : min;
+    sec= sec < 10 ? "0" + sec : sec;
+
+    const time = `${hours}:${min} ${amPm}`;
+
+    document.getElementById("clock").textContent = time;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
