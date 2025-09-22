9245eaf7ad789df41a48b793a207ab8172e82cac
samiraha1 <samiraha@email.sc.edu>
2025-09-21 22:54:08 -0400

    djbkj

--- csce242/assignments/assignment6/assignment6.js
+++ csce242/assignments/assignment6/assignment6.js
@@ -4,7 +4,7 @@ function toggleMenu() {
 
     nav.classList.toggle("active");
 
-    btn.textContent = nav.classList.contains("active") ? "▲" : "▼" ;
+    btn.textContent = nav.classList.contains("active") ? "▲" : "▼";
 }
 
 const slider = document.getElementById("plantRange");
@@ -13,52 +13,51 @@ const stat = document.getElementById("stat");
 const imagePlant = document.getElementById("plantImage");
 
 slider.oninput = () => {
-  let daysNumber = document.getElementById("plantRange").value;
+    let daysNumber = document.getElementById("plantRange").value;
 
-  days.textContent = `It's been ${daysNumber} day${
-    daysNumber !== 1 ? "s" : ""
-  } since watering your plant`;
+    days.textContent = `It's been ${daysNumber} day${daysNumber !== 1 ? "s" : ""
+        } since watering your plant`;
 
-  if (daysNumber >= 1 && daysNumber <= 2) {
-    document.getElementById("plantImage").src = "images/healthy.png";
-    stat.textContent = "Your plant is healthy and happy";
+    if (daysNumber >= 1 && daysNumber <= 2) {
+        document.getElementById("plantImage").src = "images/healthy.png";
+        stat.textContent = "Your plant is healthy and happy";
 
-  }  else if (daysNumber >= 3 && daysNumber <= 5) {
-    document.getElementById("plantImage").src = "images/watered.png";
-    stat.textContent = "Your plant needs watering";
-  } else if (daysNumber >= 6 && daysNumber <= 9) {
-    document.getElementById("plantImage").src = "images/withered.png";
-    stat.textContent = "The leaves are starting to drop, water soon!";
-  } else if (daysNumber >= 10 && daysNumber <= 12) {
-    document.getElementById("plantImage").src = "images/dead.png";
-    stat.textContent = "your plant is dead...";
-  } else {
-    // imagePlant.src = "";
-    stat.textContent = "Slide for the amount of days watered";
-    days.textContent = "Use the slider to set days";
-  }
+    } else if (daysNumber >= 3 && daysNumber <= 5) {
+        document.getElementById("plantImage").src = "images/watered.png";
+        stat.textContent = "Your plant needs watering";
+    } else if (daysNumber >= 6 && daysNumber <= 9) {
+        document.getElementById("plantImage").src = "images/withered.png";
+        stat.textContent = "The leaves are starting to drop, water soon!";
+    } else if (daysNumber >= 10 && daysNumber <= 12) {
+        document.getElementById("plantImage").src = "images/dead.png";
+        stat.textContent = "your plant is dead...";
+    } else {
+        // imagePlant.src = "";
+        stat.textContent = "Slide for the amount of days watered";
+        days.textContent = "Use the slider to set days";
+    }
 
-  console.log(document.getElementById("stat"));
+    console.log(document.getElementById("stat"));
 };
 
-function updateClock(){
+function updateClock() {
     const now = new Date();
 
     let hours = now.getHours();
     let min = now.getMinutes();
-    let sec = now. getSeconds();
+    let sec = now.getSeconds();
     let amPm = hours >= 12 ? "pm" : "am";
 
     hours = hours % 12;
     hours = hours ? hours : 12;
 
     min = min < 10 ? "0" + min : min;
-    sec= sec < 10 ? "0" + sec : sec;
+    sec = sec < 10 ? "0" + sec : sec;
 
     const time = `${hours}:${min} ${amPm}`;
 
     document.getElementById("clock").textContent = time;
-    
+
 }
 console.log("worked")
 updateClock();
