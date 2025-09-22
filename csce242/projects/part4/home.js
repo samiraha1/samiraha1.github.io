1fc81feb582a00946d2eba4cb572fc0277281e08
samiraha1 <samiraha@email.sc.edu>
2025-09-22 02:45:30 -0400

    svkbj

--- csce242/projects/part4/home.js
+++ csce242/projects/part4/home.js
@@ -1,7 +1,14 @@
-const toggleBtn = document.getElementById("menu-togglee");
-const newMenu = document.getElementById("nav-menu");
+// const toggleBtn = document.getElementById("menu-togglee");
+// const newMenu = document.getElementById("nav-menu");
 
-toggleBtn.addEventListener("click", () => {
-    navMenu.style.display = 
-    navMenu.style.display === "flex" ? "none" :"flex";
-})
+// document.getElementById("example1").onclick = () => {
+//     document.getElementById("plants-section").style.display = "block";
+//     document.getElementById("clock-section").style.display = "none";
+// }
+// document.getElementById("example2").onclick = () => {
+//     document.getElementById("plants-section").style.display = "none";
+//     document.getElementById("clock-section").style.display = "block";
+// }
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
