1b2fa6b06ed7a610fbb8daa605ff3c2225003176
samiraha1 <samiraha@email.sc.edu>
2025-09-28 21:45:52 -0400

    jfhbvkdnzck

--- csce242/assignments/assignment8/index.js
+++ csce242/assignments/assignment8/index.js
@@ -1,24 +1,25 @@
+
 const dogsBefore = {
     "Luna": "images/dog.jpg",
     "Bella": "images/dog2.jpg",
     "Charlie": "images/dog3.jpg",
     "Max": "images/dog4.jpg"
 };
+
 const dogsAfter = {
     "Luna": "images/dogAfter.jpg",
     "Bella": "images/dog2After.jpg",
     "Charlie": "images/dog3After.jpg",
     "Max": "images/dog4After.jpg"
 };
-console.log("Loading before image:", dogsBefore[name]);
 
 
 const image = document.getElementById("image");
 const dogAfter = document.getElementById("dogAfter");
 const title = document.getElementById("title");
-const closeImg = document.getElementById("close");
-const img = document.getElementById("img");
-const para = document.getElementById("adopt");
+const closeImg = document.getElementById("closeImg");
+const popupImg = document.getElementById("popupImg");
+
 
 for (let name in dogsBefore) {
     const dogDiv = document.createElement("div");
@@ -30,11 +31,12 @@ for (let name in dogsBefore) {
 
     const caption = document.createElement("div");
     caption.classList.add("adopt");
-    caption.textContent = `please adopt ${name}`;
-    // img.src = dogsAfter[name];
-    dogDiv.onclick = function() {
-        popup.classList.remove("hidden");
-        popupTitle.textContent = `${name} after adoption`;
+    caption.textContent = `Please adopt ${name}`;
+
+
+    dogDiv.onclick = function () {
+        dogAfter.classList.remove("hidden");
+        title.textContent = `${name} after adoption`;
         popupImg.src = dogsAfter[name];
     };
 
@@ -43,7 +45,9 @@ for (let name in dogsBefore) {
     image.appendChild(dogDiv);
 }
 
-closeImg.onclick = function() {
+
+closeImg.onclick = function () {
     dogAfter.classList.add("hidden");
 };
+
 
