e13eb2f1ebf0948d2a84b6bec4a969d71710ccb8
samiraha1 <samiraha@email.sc.edu>
2025-09-30 22:57:28 -0400

    fghjk

---
+++ csce242/assignments/assignment9/index.js
@@ -0,0 +1,32 @@
+class paintings{
+    constructor( name, artist, image, frame) {
+        this.name = name;
+        this.artist = artist;
+        this.image = image;
+        this.frame = frame;
+    }
+
+    get item() {
+        const paintSection = document.createElement("div");
+        paintSection.classList.add("art");
+        const heading = document.createElement("h4");
+        heading.innerText= this.name;
+        paintSection.appendChild(heading);
+        return paintSection;
+    }
+}
+window.onload = () => {
+    let paints = [];
+    let paintList = document.getElementById("paint-list");
+    paints.push(new paintings("The Persistance of Memory", "Salvador Dali", "images/persistance.jpg", "framed"));
+    paints.push(new paintings("Woman with a Parasol", "Claude Monet", "images/womanWithParasol.jpg", "not framed"));
+    paints.push(new paintings("Cafe Terrace at Night", "Van Gogh", "images/cafe_terrace.jpg", "framed"));
+    paints.push(new paintings("Hybrid Variegated Pink and Scarlet Spider Chrysanthemum", "Keika Hasegawa", "images/scarlet_spider.jpg", " not framed"));
+    paints.push(new paintings("The Amida Falls in the Far Reaches of the Kisokaidō Road", "Katsushika Hokusai", "images/amidaFalls.jpg", "framed"));
+
+
+    for (let i in paints){
+        paintList.append(paints[i].item);
+    }
+    
+}
