32f10b60a9f785178ffc08db13ce54dcdb839853
samiraha1 <samiraha@email.sc.edu>
2025-10-07 12:12:49 -0400

    fg

--- csce242/examples/04_js/04_js/js/slideshow.js
+++ csce242/examples/04_js/04_js/js/slideshow.js
@@ -1,1 +1,7 @@
-setInterval(()=>{},1000);
+setInterval(()=>{
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    const nextSlide = currentSlide.nextElementSibling;
+
+    currentSlide.classList.add("hidden");
+    nextSlide.classList.remove("hidden");
+},1000);
